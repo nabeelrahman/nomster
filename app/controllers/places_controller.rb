e10b5b36f35f3f6da2a3edfd722b84f5c1e04bc5
Nabeel <nabeelrahman@hotmail.com>
2017-11-29 06:32:19 +0000

    Update functionality

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,28 +2,37 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create] 
 
   def index
-   @places = Place.all
- end
-
-def new
-  @place = Place.new
-end
-
-def create
-  current_user.places.create(place_params)
-  redirect_to root_path
+    @places = Place.all
   end
 
-def show
-  @place = Place.find(params[:id])
-end
+  def new
+    @place = Place.new
+  end
+
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
 
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 
 
 end

273abb9f64151440606420d0109c45c1c6c243b6
Nabeel <nabeelrahman@hotmail.com>
2017-11-29 07:45:50 +0000

    Adding profile changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,6 +20,10 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status :forbidden
+    end
   end
 
   def update
